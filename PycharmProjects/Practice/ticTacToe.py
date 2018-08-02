2b3a7d6d69bd0366def6d2a41b832e415c75acd0
Alex Robbins <alexrrobbins@gmail.com>
2018-08-02 14:57:38 -0400

    Final touches, added instructions to play

--- PycharmProjects/Practice/ticTacToe.py
+++ PycharmProjects/Practice/ticTacToe.py
@@ -1,15 +1,26 @@
-#This program represents a tic-tac-toe game
+# This program represents a tic-tac-toe game
 
 import sys
 
+print('Welcome to tic-tac-toe!')
+print("Rules: Three X's or O's in any direction wins the game.")
+print("An invalid move will result in a skipped turn, so be careful!")
+print("Below is a diagram of the board. Type in the spot where you want to place your letter to make your move.")
+print(' ')
+print('top-L | top-M | top-R')
+print('______|_______|______')
+print('mid-L | mid-M | mid-R')
+print('______|_______|______')
+print('low-L | low-M | low-R')
+print('      |       |      ')
 
-
-#Initializes empty board
+# Initializes empty board
 theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
             'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
             'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
 
-#Prints out the board
+
+# Prints out the board
 def printBoard(board):
     print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
     print('-+-+-')
@@ -17,7 +28,8 @@ def printBoard(board):
     print('-+-+-')
     print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
 
-#Sets conditions for winning
+
+# Sets conditions for winning
 def setConditions(theBoard):
     conditionX1 = theBoard['top-L'] == theBoard['top-M'] == theBoard['top-R'] == 'X'
     conditionX2 = theBoard['mid-L'] == theBoard['mid-M'] == theBoard['mid-R'] == 'X'
@@ -39,43 +51,43 @@ def setConditions(theBoard):
     return [conditionX1, conditionX2, conditionX3, conditionX4, conditionX5, conditionX6, conditionX7, conditionX8,
             conditionO1, conditionO2, conditionO3, conditionO4, conditionO5, conditionO6, conditionO7, conditionO8]
 
-#Plays the game
+
+# Plays the game
 for num in range(int(sys.maxsize)):
-    #Switches turns
-    if num%2 == 0:
+    # Switches turns
+    if num % 2 == 0:
         turn = 'O'
     else:
         turn = 'X'
     print('It is now ' + turn + "'s turn. Move on which space?")
 
-    #Sets the move
+    # Sets the move
     move = input()
 
-    #Checks if move is valid and if it is performs move
+    # Checks if move is valid and if it is performs move
     if theBoard[move] == ('X' or 'O'):
         print('Invalid move')
         continue
     else:
         theBoard[move] = turn
 
-    #Prints the board after every move
+    # Prints the board after every move
     printBoard(theBoard)
 
-    #Resets condtions after every move to check if any are met
-    #Passes the state of the "board" to the function which sets the conditions as boolean values
-    #The function returns the booleans in a list so they can be used in the next if-elif statements
-    c = []
+    # Resets conditions after every move to check if any are met
+    # Passes the state of the "board" to the function which sets the conditions as boolean values
+    # The function returns the booleans in a list so they can be used in the next if-elif statements
     c = setConditions(theBoard)
 
-    #Checks each returned boolean from the list above to see if any conditions are met.
-    #If a condition is met, the program will print out the winner and exit.
+    # Checks each returned boolean from the list above to see if any conditions are met.
+    # If a condition is met, the program will print out the winner and exit.
     if c[0] or c[1] or c[2] or c[3] or c[4] or c[5] or c[6] or c[7]:
         print('X is the winner!')
         break
     elif c[8] or c[9] or c[10] or c[11] or c[12] or c[13] or c[14] or c[15]:
         print('O is the winner!')
         break
-    #If the board is filled and there are no winners, the game will be tied and the program will exit.
+    # If the board is filled and there are no winners, the game will be tied and the program will exit.
     elif ' ' not in theBoard.values():
         print('The game is tied.')
         break
