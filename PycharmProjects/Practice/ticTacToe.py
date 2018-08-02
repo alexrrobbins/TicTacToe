53019c8461f0e08f7a9615f0cb68c4e57e3483bc
Alex Robbins <alexrrobbins@gmail.com>
2018-08-02 13:25:46 -0400

    Initial commit, start of game

---
+++ PycharmProjects/Practice/ticTacToe.py
@@ -0,0 +1,18 @@
+#This program represents a tic-tac-toe game
+
+#Initializes empty board
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+
+#Prints out the board
+def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+printBoard(theBoard)
+
+global turn = 'X' #sets the turn
+print('It is now ' + turn + "'s turn. Move on which space?")
