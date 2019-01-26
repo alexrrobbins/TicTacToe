f4c7f0f107e5ce4b6fb1328021762df6e4e2caa7
Alex Robbins <alexrrobbins@gmail.com>
2019-01-26 16:04:07 -0500

    Removed excess directories, fixed a few issues with Linux

--- ticTacToe.py
+++ ticTacToe.py
@@ -11,53 +11,53 @@ Below is a diagram of the board. Type in the spot where you want to place your l
 
 ''')
 
-print('top-L | top-M | top-R')
-print('______|_______|______')
-print('mid-L | mid-M | mid-R')
-print('______|_______|______')
-print('low-L | low-M | low-R')
-print('      |       |      ')
+print('topL | topM | topR')
+print('_____|______|______')
+print('midL | midM | midR')
+print('_____|______|______')
+print('lowL | lowM | lowR')
+print('     |      |      ')
 
 # Initializes empty board
-theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
-            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
-            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+theBoard = {'topL': ' ', 'topM': ' ', 'topR': ' ',
+            'midL': ' ', 'midM': ' ', 'midR': ' ',
+            'lowL': ' ', 'lowM': ' ', 'lowR': ' '}
 
 
 # Prints out the board
 def printBoard(board):
-    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print(board['topL'] + '|' + board['topM'] + '|' + board['topR'])
     print('-+-+-')
-    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print(board['midL'] + '|' + board['midM'] + '|' + board['midR'])
     print('-+-+-')
-    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+    print(board['lowL'] + '|' + board['lowM'] + '|' + board['lowR'])
 
 
 # Sets conditions for winning
 def setConditions(theBoard):
-    conditionX1 = theBoard['top-L'] == theBoard['top-M'] == theBoard['top-R'] == 'X'
-    conditionX2 = theBoard['mid-L'] == theBoard['mid-M'] == theBoard['mid-R'] == 'X'
-    conditionX3 = theBoard['low-L'] == theBoard['low-M'] == theBoard['low-R'] == 'X'
-    conditionX4 = theBoard['top-L'] == theBoard['mid-M'] == theBoard['low-R'] == 'X'
-    conditionX5 = theBoard['top-R'] == theBoard['mid-M'] == theBoard['low-L'] == 'X'
-    conditionX6 = theBoard['top-L'] == theBoard['mid-L'] == theBoard['low-L'] == 'X'
-    conditionX7 = theBoard['top-M'] == theBoard['mid-M'] == theBoard['low-M'] == 'X'
-    conditionX8 = theBoard['top-R'] == theBoard['mid-R'] == theBoard['low-R'] == 'X'
+    conditionX1 = theBoard['topL'] == theBoard['topM'] == theBoard['topR'] == 'X'
+    conditionX2 = theBoard['midL'] == theBoard['midM'] == theBoard['midR'] == 'X'
+    conditionX3 = theBoard['lowL'] == theBoard['lowM'] == theBoard['lowR'] == 'X'
+    conditionX4 = theBoard['topL'] == theBoard['midM'] == theBoard['lowR'] == 'X'
+    conditionX5 = theBoard['topR'] == theBoard['midM'] == theBoard['lowL'] == 'X'
+    conditionX6 = theBoard['topL'] == theBoard['midL'] == theBoard['lowL'] == 'X'
+    conditionX7 = theBoard['topM'] == theBoard['midM'] == theBoard['lowM'] == 'X'
+    conditionX8 = theBoard['topR'] == theBoard['midR'] == theBoard['lowR'] == 'X'
 
-    conditionO1 = theBoard['top-L'] == theBoard['top-M'] == theBoard['top-R'] == 'O'
-    conditionO2 = theBoard['mid-L'] == theBoard['mid-M'] == theBoard['mid-R'] == 'O'
-    conditionO3 = theBoard['low-L'] == theBoard['low-M'] == theBoard['low-R'] == 'O'
-    conditionO4 = theBoard['top-L'] == theBoard['mid-M'] == theBoard['low-R'] == 'O'
-    conditionO5 = theBoard['top-R'] == theBoard['mid-M'] == theBoard['low-L'] == 'O'
-    conditionO6 = theBoard['top-L'] == theBoard['mid-L'] == theBoard['low-L'] == 'O'
-    conditionO7 = theBoard['top-M'] == theBoard['mid-M'] == theBoard['low-M'] == 'O'
-    conditionO8 = theBoard['top-R'] == theBoard['mid-R'] == theBoard['low-R'] == 'O'
+    conditionO1 = theBoard['topL'] == theBoard['topM'] == theBoard['topR'] == 'O'
+    conditionO2 = theBoard['midL'] == theBoard['midM'] == theBoard['midR'] == 'O'
+    conditionO3 = theBoard['lowL'] == theBoard['lowM'] == theBoard['lowR'] == 'O'
+    conditionO4 = theBoard['topL'] == theBoard['midM'] == theBoard['lowR'] == 'O'
+    conditionO5 = theBoard['topR'] == theBoard['midM'] == theBoard['lowL'] == 'O'
+    conditionO6 = theBoard['topL'] == theBoard['midL'] == theBoard['lowL'] == 'O'
+    conditionO7 = theBoard['topM'] == theBoard['midM'] == theBoard['lowM'] == 'O'
+    conditionO8 = theBoard['topR'] == theBoard['midR'] == theBoard['lowR'] == 'O'
     return [conditionX1, conditionX2, conditionX3, conditionX4, conditionX5, conditionX6, conditionX7, conditionX8,
             conditionO1, conditionO2, conditionO3, conditionO4, conditionO5, conditionO6, conditionO7, conditionO8]
 
 
 # Plays the game
-for num in range(int(sys.maxsize)):
+for num in range(0, 999):
     # Switches turns
     if num % 2 == 0:
         turn = 'O'
@@ -66,12 +66,11 @@ for num in range(int(sys.maxsize)):
     print('It is now ' + turn + "'s turn. Move on which space?")
 
     # Sets the move
-    move = input()
+    move = str( input() )
 
     # Checks if move is valid and if it is performs move
     if theBoard[move] == ('X' or 'O'):
         print('Invalid move')
-        continue
     else:
         theBoard[move] = turn
 
@@ -87,11 +86,11 @@ for num in range(int(sys.maxsize)):
     # If a condition is met, the program will print out the winner and exit.
     if c[0] or c[1] or c[2] or c[3] or c[4] or c[5] or c[6] or c[7]:
         print('X is the winner!')
-        break
+        sys.exit()
     elif c[8] or c[9] or c[10] or c[11] or c[12] or c[13] or c[14] or c[15]:
         print('O is the winner!')
-        break
+        sys.exit()
     # If the board is filled and there are no winners, the game will be tied and the program will exit.
     elif ' ' not in theBoard.values():
         print('The game is tied.')
-        break
+        sys.exit()
